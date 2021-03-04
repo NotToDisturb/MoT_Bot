7b8b0e9a4d6dfeea57b0af99acda096ec837fe60
NotToDisturb <nottodisturb.disturbo@gmail.com>
2021-03-04 18:24:36 +0100

    Implementation of poke command

--- command.py
+++ command.py
@@ -1,5 +1,8 @@
 import csv
 import os
+
+import discord
+
 import sheets_utils
 
 from dotenv import load_dotenv
@@ -39,7 +42,28 @@ class Command:
             await self.message.channel.send("Not enough permission to rebuild CSV files")
 
     async def poke(self):
-        pass
+        index = sheets_utils.get_index_in_column(CSV_POKES, "Name", self.args)
+        if index > -1:
+            pokedex_num = sheets_utils.get_element_in_column(CSV_POKES, "Num", index)
+            image = sheets_utils.get_element_in_column(CSV_POKES, "Image", index)
+            notes = sheets_utils.get_element_in_column(CSV_POKES, "Notes", index)
+            sources = sheets_utils.get_element_in_column(CSV_POKES, "Sources", index).split("|")
+
+            embed_message = discord.Embed(title="Nº" + str(pokedex_num) + " - " + self.args, color=0x52307c)
+            embed_message.set_thumbnail(url=image)
+
+            if len(sources) > 0:
+                sources_value = ""
+                for source_index in range(0, len(sources)):
+                    source_name = sheets_utils.get_element_in_column(CSV_SOURCES, "Name", int(sources[source_index]))
+                    source_value = sheets_utils.get_element_in_column(CSV_SOURCES, "Link", int(sources[source_index]))
+                    sources_value += "\n[" + source_name +  "](" + source_value + ")"
+                embed_message.add_field(name="Sources", value=sources_value, inline=False)
+            if len(notes) > 0:
+                embed_message.add_field(name="Dev notes", value=notes, inline=False)
+            await self.message.channel.send(embed=embed_message)
+        else:
+            await self.message.channel.send(self.args + " is not in the Avlarian Pokédex")
 
 
 cmd_to_func = {"csv": Command.generate_csv,
